ae88c99869b7b32640bb5c206bd7ec15c0d37996
Kudoh Satoshi <kudoh_satoshi@ics.co.jp>
2026-01-14 06:33:22 +0000

    implement pad gen

--- src/Pad.tsx
+++ src/Pad.tsx
@@ -2,21 +2,38 @@ import React from 'react';
 
 import './Pad.css';
 
+const API_URL: string = 'http://evm.ics.co.jp:8088/PadGen/api/pdl/form'
+
 export const Pad = (Tag: React.FunctionComponent<any>): React.FunctionComponent<any> => {
   return ({ children, className, ...props }) => {
     try {
       if (className !== 'language-pad') {
         return (
-          <Tag {...props}>{children} NOT pad, className={className}, children={children}, props={...props}</Tag>
+          <Tag {...props}>{children}</Tag>
         );
       }
-      return (
-        // <Tag {...props}>{children} this is pad, className={className}</Tag>
-        <ul>
-        <li>{className}</li>
-        <li>{children}</li>
-        </ul>
-      );
+
+      const layoutName = 'コンパクト';
+
+      fetch(API_URL, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json;charset=UTF-8'
+          },
+          body: JSON.stringify({ LayoutName: layoutName, Text: children })
+      })
+      .then((response: Response) => {
+          if (!response.ok) {
+              return 'net work resonse was not ok';
+          }
+          return response.text();
+      })
+      .then((result: string) => {
+          return result;
+      })
+      .catch((error: Error) => {
+          return 'error: ' + error;
+      });
     }
     catch (err) {
       return (
