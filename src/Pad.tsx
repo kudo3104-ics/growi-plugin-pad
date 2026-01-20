47445a9d9d3400c656b717c46c3d4d1d0530e72d
Kudoh Satoshi <kudoh_satoshi@ics.co.jp>
2026-01-20 02:32:45 +0000

    use useEffect

--- src/Pad.tsx
+++ src/Pad.tsx
@@ -1,57 +1,58 @@
-import React from 'react';
-// import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './Pad.css';
 
 const API_URL: string = 'http://10.20.40.64:8070/api/pdl/form'
 
 export const Pad = (Tag: React.FunctionComponent<any>): React.FunctionComponent<any> => {
-  // const [svgContent, setSvgContent] = useState<string | null>(null);
-  // const [error, setError] = useState<string | null>(null);
-
   return ({ children, className, ...props }) => {
-    try {
-      if (className !== 'language-pad') {
-        return (
-          <Tag {...props}>{children}</Tag>
-        );
-      }
-      // return (<div>hoge {children} hoge</div>);
+    const [svgContent, setSvgContent] = useState<string | null>(null);
+    const [error, setError] = useState<string | null>(null);
 
-      const layoutName = 'コンパクト';
-
-      fetch(API_URL, {
+    useEffect(() => {
+      if (className === 'language-pad') {
+        const layoutName = 'コンパクト';
+        fetch(API_URL, {
           method: 'POST',
           headers: {
               'Content-Type': 'application/json;charset=UTF-8'
           },
           body: JSON.stringify({ LayoutName: layoutName, Text: children })
-      })
-      .then((response: Response) => {
+        })
+        .then((response: Response) => {
           if (!response.ok) {
-              console.log('net work resonse was not ok');
-              return 'net work resonse was not ok';
+              console.log('network resonse was not ok');
+              throw new Error('network response was not ok');
           }
-          // console.log('response was ok');
           return response.text();
-      })
-      .then((svg: string) => {
+        })
+        .then((svg: string) => {
           console.log('svg!');
-          return (<div>{svg}</div>);
-      })
-      .catch((error: Error) => {
+          setSvgContent(svg);
+        })
+        .catch((error: Error) => {
           console.log('error');
           console.log(error);
-          return 'error: ' + error;
-      });
-    }
-    catch (err) {
-      console.log('error2');
-      console.log(err);
+          setError(error.message);
+        });
+      }
+    }, [children, className]);
+
+    if (className !== 'language-pad') {
       return (
-        <Tag {...props}>{children} err</Tag>
+        <Tag {...props}>{children}</Tag>
       );
     }
+
+    if (error) {
+      return <div style={{ color: 'red' }}>svg fetch error: {error}</div>;
+    }
+
+    if (svgContent === null) {
+      return <div>svg fetch now...</div>;
+    }
+
+    return (svgContent);
   };
 };
 
