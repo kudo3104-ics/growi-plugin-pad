2f401920034e011044ecac555384bfeafbf3e35a
Kudoh Satoshi <kudoh_satoshi@ics.co.jp>
2026-01-20 04:04:10 +0000

    change to HOC

--- src/Pad.tsx
+++ src/Pad.tsx
@@ -4,11 +4,11 @@ import './Pad.css';
 
 const API_URL: string = 'http://10.20.40.64:8070/api/pdl/form'
 
-export const Pad = (Tag: React.FunctionComponent<any>): React.FunctionComponent<any> => {
+export const Pad = (Tag: React.FC<any>): React.FC<any> => {
   return ({ children, className, ...props }) => {
     const [svgContent, setSvgContent] = useState<string | null>(null);
     const [error, setError] = useState<string | null>(null);
-
+  
     useEffect(() => {
       if (className === 'language-pad') {
         const layoutName = 'コンパクト';
@@ -37,22 +37,21 @@ export const Pad = (Tag: React.FunctionComponent<any>): React.FunctionComponent<
         });
       }
     }, [children, className]);
-
+  
     if (className !== 'language-pad') {
       return (
         <Tag {...props}>{children}</Tag>
       );
     }
-
+  
     if (error) {
       return <div style={{ color: 'red' }}>svg fetch error: {error}</div>;
     }
-
+  
     if (svgContent === null) {
       return <div>svg fetch now...</div>;
     }
-
-    return 'its svg!'; //(svgContent);
+  
+    return svgContent;
   };
 };
-
